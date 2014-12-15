2a6bd613df81fbf4e22c067ef5632c3ddbdf1080
derek <deeayp@gmail.com>
2014-12-14 22:04:53 -0800

    Durp...

--- SuperBlockPush/box.cpp
+++ SuperBlockPush/box.cpp
@@ -37,11 +37,11 @@ void box::collides(bool set[200], box boxes[200], int move[2], bool& flag, int p
 	for (int i = 0; i < 200; i ++) {
 		for (int j = 0; j < 7; j++) {
 			for (int k = 0; k < 7; k++) {
-				int tmpCords1[2] = {coordinates[0] - 3 + j, coordinates[1] - 3 + k};
+				int tmpCords1[2] = {coordinates[0] - 3 + k, coordinates[1] - 3 + j};
 				int tmpCords2[2] = {tmpCords1[0] + move[0], tmpCords1[1] + move[1]};
-				bool tmpBool1 = occupied(tmpCords1);
-				bool tmpBool2 = boxes[i].occupied(tmpCords2);
-				if (tmpBool1 && tmpBool2 && i < pointer) {
+				bool tmpBool1 = occupied(tmpCords1); //Tests if the currently chacking block occupies a space
+				bool tmpBool2 = boxes[i].occupied(tmpCords2); //Tests the same space displaced by the movement, where the block will be.
+				if (tmpBool1 && tmpBool2 && i != pointer) {
 					set[i] = true;
 					boxes[i].collides(set, boxes, move, flag, i);
 					
